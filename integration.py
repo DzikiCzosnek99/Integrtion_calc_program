9aadc87384d19e2ccb478aa524a202d062947c57
DzikiCzosnek99 <jonren@o2.pl>
2021-12-15 20:55:14 +0100

    Add Python file

---
+++ integration.py
@@ -0,0 +1,33 @@
+from math import *
+import numpy as np
+
+
+class Integration:
+    def __init__(self, func, lower_limit, upper_limit, time_step=None):
+        self.func = func
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
+        self.time_step = time_step
+        if self.time_step is None:
+            self.time_step = float(abs(self.upper_limit - self.lower_limit) / 100)
+
+    def return_func(self, x):
+        copy = self.func
+        copy = copy.replace('x', str(x))
+        copy = copy.replace('^', '**')
+        return eval(copy)
+
+    def calculation(self,h):
+        result = self.return_func(self.lower_limit) + self.return_func(self.upper_limit)
+        x = self.lower_limit = h
+        while x < self.upper_limit:
+            result = result + 2 * self.return_func(x)
+            x = round(x+h,12)
+        result = result * 0.5 * h
+        return result
+    def results(self):
+        return self.calculation(h=self.time_step) + (1 / 3) * (self.calculation(h=self.time_step) - self.calculation(h=2 * self.time_step))
+
+
+test = Integration('x^3 + x', 0, 5,0.001)
+print(test.results())
